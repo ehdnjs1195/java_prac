11cde2aa7e8f9f2eedb99bfa0469609c1d8a1701
ADW <ehdnjs1195@naver.com>
2023-04-11 17:13:37 +0900

    Clock 만들기
    Functions 클래스 만들고, ttl7447()메서드 정의함.

--- Clock/src/alarmClock/Clock.java
+++ Clock/src/alarmClock/Clock.java
@@ -3,24 +3,18 @@ package alarmClock;
 public class Clock {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Functions f = new Functions();
+		int[] num = new int[4];
+		char fnd[];
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 4; j++) {
+				num[j] = ((i >> j) & 1);
+			}
+			fnd = f.ttl7447(num);
+			f.dispFND(fnd);
+		}
 		
-	    char[][] fnd = {{1, 1, 1, 1, 1, 1, 0}, //0
-	            {0, 1, 1, 0, 0, 0, 0}, 
-	            {1, 1, 0, 1, 1, 0, 1}, 
-	            {1, 1, 1, 1, 0, 0, 1}, 
-	            {0, 1, 1, 0, 0, 1, 1}, 
-	            {1, 0, 1, 1, 0, 1, 1}, 
-	            {1, 0, 1, 1, 1, 1, 1}, 
-	            {1, 1, 1, 0, 0, 0, 0}, 
-	            {1, 1, 1, 1, 1, 1, 1}, 
-	            {1, 1, 1, 0, 0, 1, 1}};
-	    
-	    for(int i=0; i<10; i++) {
-	    	f.dispFND(fnd[i]);
-	    }
 	}
-	
-	
+
 }
--- Clock/src/alarmClock/Functions.java
+++ Clock/src/alarmClock/Functions.java
@@ -3,10 +3,10 @@ package alarmClock;
 public class Functions {
 	public void dispFND(char[] fnd) {
 		char[][] seg = new char[5][5];
-		for(int i=0; i<5; i++) 
-			for(int j=0; j<5; j++)
+		for (int i = 0; i < 5; i++)
+			for (int j = 0; j < 5; j++)
 				seg[i][j] = ' ';
-		
+
 		if(fnd[0] == 1) for(int j=0; j<5;j++) seg[0][j] = '#';
 		if(fnd[1] == 1) for(int j=0; j<3;j++) seg[j][4] = '#';
 		if(fnd[2] == 1) for(int j=0; j<3;j++) seg[2+j][4] = '#';
@@ -15,14 +15,34 @@ public class Functions {
 		if(fnd[5] == 1) for(int j=0; j<3;j++) seg[j][0] = '#';
 		if(fnd[6] == 1) for(int j=0; j<5;j++) seg[2][j] = '#';
 		
-		for(int i=0; i<seg.length; i++) {
+		for (int i = 0; i < seg.length; i++) {
 			System.out.println(seg[i]);
 		}
 		System.out.println("");
 	}
 	
-	public void ttl7447(char[] fnd) {
-		
+	public char[] ttl7447(int[] num) {
+		char[][] fnd = {
+				{1, 1, 1, 1, 1, 1, 0}, //0
+	            {0, 1, 1, 0, 0, 0, 0}, //1
+	            {1, 1, 0, 1, 1, 0, 1}, //2
+	            {1, 1, 1, 1, 0, 0, 1}, //3
+	            {0, 1, 1, 0, 0, 1, 1}, //4
+	            {1, 0, 1, 1, 0, 1, 1}, //5
+	            {1, 0, 1, 1, 1, 1, 1}, //6
+	            {1, 1, 1, 0, 0, 0, 0}, //7
+	            {1, 1, 1, 1, 1, 1, 1}, //8
+	            {1, 1, 1, 0, 0, 1, 1}};//9
+		int n = 0;
+		for (int i = 0; i < 4; i++) {
+			n |= (num[i] << i);
+		}
+		return fnd[n];
 	}
 
+	public char[] ttl7490(int clk) {
+		
+		
+		return ;
+	}
 }
